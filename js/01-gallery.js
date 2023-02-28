31572cfa65743f8b14dcdda9efaffca857596074
VladyslavYakivets <aquartis97@gmail.com>
2023-02-28 23:22:51 +0200

    домашня робота №2

---
+++ js/01-gallery.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const items = galleryItems.map(item => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`}).join('');
+
+  const galleryBox = document.querySelector('.gallery');
+  galleryBox.innerHTML = items;
+
+  galleryBox.addEventListener('click', onLinkClick);
+
+  function onLinkClick(event){
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img width="1400" height="900" src="${event.target.dataset.source}">
+	`);
+	instance.show()
+
+  };
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,18 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const items = galleryItems.map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`}).join("");
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+
+
+let lightbox = new SimpleLightbox('.gallery a', { caption: true, captionSelector: 'img[alt]', captionType: 'attr', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 }); 
+
+
+
+
+
+console.log(galleryItems);
